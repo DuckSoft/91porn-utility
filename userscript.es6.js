cc2471a8d7cf86cb95020b3748907fc8e0429c58
DuckSoft <ducksoft@foxmail.com>
2018-12-02 10:32:52 +0800

    Update userscript.es6.js

--- userscript.es6.js
+++ userscript.es6.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         91Porn Video Address Extractor
 // @namespace    http://github.com/DuckSoft/
-// @version      0.1
+// @version      0.2
 // @description  Double-click to get the address!
 // @author       DuckSoft
 // @match        *://91porn.com/view_video.php?*
@@ -12,7 +12,7 @@
     'use strict';
     document.addEventListener('dblclick', () => {
         let sources = document.getElementsByTagName("source");
-        if (!sources) alert("No video source found");
+        if (!sources.length) alert("No video source found. Maybe still loading");
         else window.location = sources[0].src;
     });
 })();
