7bdc8d5a0e871605b99b3f0345f8a14c11a42152
DuckSoft <ducksoft@foxmail.com>
2019-05-08 08:54:10 +0800

    清除顶栏浮动广告 (#4)
    This shall fix #4.

--- src/util/SanityHelper.ts
+++ src/util/SanityHelper.ts
@@ -1,7 +1,11 @@
 export function removeAds() {
+  // 清除 iframe 广告，但不破坏页面结构
   document.querySelectorAll("iframe").forEach(e => {
     if (e.src.includes("fans.91p20.space/fans/")) {
       e.replaceWith("广告已移除owo")
     }
   });
+
+  // 清除顶栏浮动广告
+  document.querySelectorAll("#topbar").forEach(e => e.remove());
 }
